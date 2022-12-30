ec604d8e16a77c5a5144b2bd689bc6e420632f81
NEX <veperx@icloud.com>
2022-12-31 07:30:17 +0800

    UPDATE

--- init.lua
+++ init.lua
@@ -21,38 +21,14 @@ require "core.filetype"     -- ~/nvim/lua/core/filetype.lua
 --   end)
 -- end)
 
--- api.nvim_command("packadd impatient.nvim")
--- require          "impatient"
-
--- ; echo 1 > /home/nex/hello
-
 vim.defer_fn(function ()
     require "core.options"     -- ~/nvim/lua/core/options.lua
     require "core.keymaps"     -- ~/nvim/lua/core/keymaps.lua
     require "core.autocmd"     -- ~/nvim/lua/core/autocmd.lua
     require "plugins"          -- ~/nvim/lua/plugins/init.lua
 
-    -- require "plug-manager"     -- ~/nvim/lua/plug-manager/init.lua
-
     vim.o.shadafile = ""
 
-    -- api.nvim_exec ([[
-    --     packadd matchparen.nvim
-    --     packadd nvim-colorizer.lua
-    --
-    --     packadd nvim-treesitter-textobjects
-    --
-    --     packadd matchit
-    --     let b:match_ignorecase = 0
-    --     let b:match_words =
-    --         \ '\<\%(do\|function\|if\)\>:' ..
-    --         \ '\<\%(return\|else\|elseif\)\>:' ..
-    --         \ '\<end\>,' ..
-    --         \ '\<repeat\>:\<until\>,' ..
-    --         \ '\%(--\)\=\[\(=*\)\[:]\1]'
-    -- ]], false)
-    -- require "matchparen".setup()
-    -- require "colorizer".setup({ "*" }, { mode = "foreground" })
 end, 100)
 
 -- vim.defer_fn(function ()
@@ -65,14 +41,6 @@ end, 100)
 --     -- require "plug-manager.treesitter"  -- ~/nvim/lua/plug-manager/treesitter.lua
 --
 -- end, 10)
-
--- USE LOCAL VAR!!!
--- VIML SPEED    : api.nvim_command < api.nvim_exec(xxx, false) < api.nvim_exec(xxx, true) < vim.cmd < vim.cmd.xxx < others
--- VIMFUNC SPEED : vim.call("xxx")  = vim.fn.xxx = vim.fn['xxx'] = api.nvim_call_function
--- KEYMAP rhs    : gt < vim.cmd.xxx < <cmd>xxx<CR>
--- OPTION set    : vim.o.xxx < map + true < api.nvim_command < map + ipairs
--- KEYMAP set    : api.nvim_set_keymap + table !!! termcode can't use in nvim_set_keymap
--- var           : api.nvim_buf_get_option < vim.bo.xxx
 
 -- vim.keymap.set("n", ";d", function ()
 --     local time = os.clock()
